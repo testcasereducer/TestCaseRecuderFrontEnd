a5e45fea95602809cf13caf34235ce8622d77d9d
testcasereducer <testcasereducer@gmail.com>
2023-05-15 11:46:01 -0500

    Modificando env

--- components/utilities/env.js
+++ components/utilities/env.js
@@ -1,2 +1,2 @@
-export const API_URL='https://api-testcasereducer.onrender.com'
+export const API_URL='https://api-testcasereducer.onrender.com/api'
 export const API_KEY='6765696c657268697069616d656a69613040686f746d61696c2e636f6d5f0a2ce9262961d27787b161a9e4c6b9'
