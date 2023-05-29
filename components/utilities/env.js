312184b264934eb92d7674c33cf5496a51c2b43e
geiler.hipia <geiler.hipia@sophossolutions.com>
2023-05-29 11:52:03 -0500

    Agregando API_URL

--- components/utilities/env.js
+++ components/utilities/env.js
@@ -1,2 +1,2 @@
-export const API_URL= 'http://localhost:8000/api'//'https://api-testcasereducer.onrender.com/api'
+export const API_URL= 'https://api-testcasereducer.onrender.com/api'
 export const API_KEY='a61802bbc5fdaf52355a283c83e43fc4'
